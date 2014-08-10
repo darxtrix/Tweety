5a8ca5146857919a94eb679dc60c55e190bf5710
Ankush Sharma <ankprashar@gmail.com>
2014-08-11 00:06:17 +0530

    gunicorn pointed to correct url and fixed issues with settings.py

--- tweety/settings.py
+++ tweety/settings.py
@@ -9,7 +9,7 @@ PROJECT_DIR = os.path.dirname(__file__)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
