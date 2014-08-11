89da8f739e2521df71eff7b48867e6f970f2d049
Ankush Sharma <ankprashar@gmail.com>
2014-08-12 01:21:57 +0530

    again a typo

--- tweety/wsgi_production.py
+++ tweety/wsgi_production.py
@@ -3,7 +3,7 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweety.settings.product")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tweety.settings.production")
 
 application = get_wsgi_application()
 application = Cling(get_wsgi_application())
