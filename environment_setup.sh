f48e89343a59b1020711a38d3eae0bc2abee1cd0
black-perl <ankush.sharma.ece12@itbhu.ac.in>
2014-08-13 03:37:12 +0530

    updated .gitignore

--- environment_setup.sh
+++
@@ -1,4 +0,0 @@
-export SECRET_KEY='ixz99s)r0=fza2apc-7ggpx%_25!vh77^_-_5^us&9lx6yx7-#';
-export API_KEY='CUIDTqUwjbCwKI0N4aAK8suEM';
-export API_SECRET='St7GnFXpQVIsWhW5KhTZAXEDbsnEaMqnu1G7PEy6Pahw8eknBO';
-export PYTHONUNBUFFERED=True;
