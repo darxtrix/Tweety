37039ee2d53e2f31e29fea162b5121ab920c5282
black-perl <ankush.sharma.ece12@itbhu.ac.in>
2014-10-29 22:33:36 +0530

    Added the correct import commands

--- tweety/urls.py
+++ tweety/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 import views
+from django.http import HttpResponse
 #from django.contrib import admin
 #admin.autodiscover()
 
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^$',views.index),
     url(r'^index/$',views.index),
     url(r'^get_tweets/$',views.get_tweets),
+    url(r'^google4962e1a518060429\.html$', lambda r: HttpResponse("google-site-verification: google4962e1a518060429.html", mimetype="text/plain")),
 )
