5ed75ec5e6a8d8dc511ada017fb86e8876436137
black-perl <ankush.sharma.ece12@itbhu.ac.in>
2014-10-29 23:52:38 +0530

    added serving code for the robots.txt file and sitemap file

--- tweety/urls.py
+++ tweety/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 import views
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 #from django.contrib import admin
 #admin.autodiscover()
 
@@ -9,5 +10,7 @@ urlpatterns = patterns('',
     url(r'^$',views.index),
     url(r'^index/$',views.index),
     url(r'^get_tweets/$',views.get_tweets),
-    url(r'^google4962e1a518060429\.html$', lambda r: HttpResponse("google-site-verification: google4962e1a518060429.html", mimetype="text/plain")),
+    url(r'^google4962e1a518060429\.html$', lambda r: HttpResponse("google-site-verification: google4962e1a518060429.html", mimetype="text/plain")), #added google site ownership verification file
+	url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt')), #added ronots.txt file
+	url(r'^sitemap.xml$',TemplateView.as_view(template_name='sitemap.xml')), #serving sitemap
 )
